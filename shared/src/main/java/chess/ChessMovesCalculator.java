27dde29d96d9871b4f29dd8bf6dc24be24e2cb56
hiattj19 <hiattj19@gmail.com>
2025-01-17 16:28:22 -0700

    Created interface ChessMovesCalculator

---
+++ shared/src/main/java/chess/ChessMovesCalculator.java
@@ -0,0 +1,10 @@
+package chess;
+
+public interface ChessMovesCalculator {
+    void KingsMoveCalculator();
+    void QueensMoveCalculator();
+    void BishopsMoveCalculator();
+    void PawnMoveCalculator();
+    void KnightsMoveCalculator();
+    void RooksMoveCalculator();
+}
