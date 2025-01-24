f53ce6893b64c128f6e47a439cef555e0c7c95c0
hiattj19 <hiattj19@gmail.com>
2025-01-23 17:03:48 -0700

    Created BishopsMoveCalc and passed 1 test.

--- shared/src/main/java/chess/BishopsMoveCalculator.java
+++ shared/src/main/java/chess/BishopsMoveCalculator.java
@@ -2,7 +2,7 @@ package chess;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
+import java.util.Objects;
 
 public class BishopsMoveCalculator implements ChessMovesCalculator{
     private final ChessBoard board;
@@ -14,6 +14,119 @@ public class BishopsMoveCalculator implements ChessMovesCalculator{
         this.position = position;
     }
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
-        return List.of();
+        if (board.getPiece(position).getTeamColor().equals(ChessGame.TeamColor.WHITE)) {
+            diagonalUpRight(position);
+            diagonalDownRight(position);
+            diagonalDownLeft(position);
+            diagonalUpLeft(position);
+        }
+        else {
+            diagonalDownRight(position);
+            diagonalDownLeft(position);
+            diagonalUpLeft(position);
+            diagonalUpRight(position);
+        }
+        return legalMoves;
+    }
+
+    public ChessPosition diagonalUpRight(ChessPosition startPos){
+        if (startPos.getRow() >= 8 || startPos.getColumn() >= 8){
+            System.out.println("Hit Edge: " + startPos.getRow() + " " + startPos.getColumn());
+            return startPos;
+        }
+        int tempR = startPos.getRow() + 1;
+        int tempC = startPos.getColumn() + 1;
+        ChessPosition newPos = new ChessPosition(tempR,tempC);
+        if (board.getPiece(newPos) == null){
+            legalMoves.add(new ChessMove(startPos,newPos,null));
+            System.out.println("DUR");
+            return diagonalUpRight(newPos);
+        }
+        else if (board.getPiece(newPos) != null){
+            if (board.getPiece(newPos).getTeamColor() != board.getPiece(position).getTeamColor()){
+                legalMoves.add(new ChessMove(startPos, newPos, null));
+                System.out.println("DURx");
+            }
+        }
+        return newPos;
+    }
+    public ChessPosition diagonalUpLeft(ChessPosition startPos){
+        if (startPos.getRow() >= 8 || startPos.getColumn() <= 1){
+            System.out.println("Hit Edge: " + startPos.getRow() + " " + startPos.getColumn());
+            return startPos;
+        }
+        int tempR = startPos.getRow() + 1;
+        int tempC = startPos.getColumn() - 1;
+        ChessPosition newPos = new ChessPosition(tempR,tempC);
+        if (board.getPiece(newPos) == null){
+            legalMoves.add(new ChessMove(startPos,newPos,null));
+            System.out.println("DUL");
+            return diagonalUpLeft(newPos);
+        }
+        else if (board.getPiece(newPos) != null){
+            if (board.getPiece(newPos).getTeamColor() != board.getPiece(position).getTeamColor()){
+                legalMoves.add(new ChessMove(startPos, newPos, null));
+                System.out.println("DULx");
+            }
+        }
+        return newPos;
+    }
+
+    public ChessPosition diagonalDownRight(ChessPosition startPos){
+        if (startPos.getRow() <= 1 || startPos.getColumn() >= 8){
+            System.out.println("Hit Edge: " + startPos.getRow() + " " + startPos.getColumn());
+            return startPos;
+        }
+        int tempR = startPos.getRow() - 1;
+        int tempC = startPos.getColumn() + 1;
+        ChessPosition newPos = new ChessPosition(tempR,tempC);
+        if (board.getPiece(newPos) == null){
+            legalMoves.add(new ChessMove(startPos,newPos,null));
+            System.out.println("DDR");
+            return diagonalDownRight(newPos);
+        }
+        else if (board.getPiece(newPos) != null){
+            if (board.getPiece(newPos).getTeamColor() != board.getPiece(position).getTeamColor()){
+                legalMoves.add(new ChessMove(startPos, newPos, null));
+                System.out.println("DDRx");
+            }
+        }
+        return newPos;
+    }
+
+    public ChessPosition diagonalDownLeft(ChessPosition startPos){
+        if (startPos.getRow() <= 1 || startPos.getColumn() <= 1){
+            System.out.println("Hit Edge: " + startPos.getRow() + " " + startPos.getColumn());
+            return startPos;
+        }
+        int tempR = startPos.getRow() - 1;
+        int tempC = startPos.getColumn() - 1;
+        ChessPosition newPos = new ChessPosition(tempR,tempC);
+        if (board.getPiece(newPos) == null){
+            legalMoves.add(new ChessMove(startPos,newPos,null));
+            System.out.println("DDL");
+            return diagonalDownLeft(newPos);
+        }
+        else if (board.getPiece(newPos) != null){
+            if (board.getPiece(newPos).getTeamColor() != board.getPiece(position).getTeamColor()){
+                legalMoves.add(new ChessMove(startPos, newPos, null));
+                System.out.println("DDLx");
+            }
+        }
+        return newPos;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        BishopsMoveCalculator that = (BishopsMoveCalculator) o;
+        return Objects.equals(board, that.board) && Objects.equals(position, that.position) && Objects.equals(legalMoves, that.legalMoves);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(board, position, legalMoves);
     }
 }
