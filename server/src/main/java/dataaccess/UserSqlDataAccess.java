1b62953e6aab0e22736b111822e9b49d9e957e6d
hiattj19 <hiattj19@gmail.com>
2025-03-12 14:03:58 -0600

    Added the DataBaseManager and tests, beginning work on UserSqlDataAccess

--- server/src/main/java/dataaccess/UserSqlDataAccess.java
+++ server/src/main/java/dataaccess/UserSqlDataAccess.java
@@ -1,5 +1,52 @@
 package dataaccess;
 
 
-public class UserSqlDataAccess{
+import exception.ResponseException;
+import model.UserData;
+
+import java.sql.SQLDataException;
+import java.sql.SQLException;
+
+public class UserSqlDataAccess implements UserDAO{
+
+    public UserSqlDataAccess() {
+        configureDatabase();
+    }
+    @Override
+    public UserData createUser(UserData user) {
+        return null;
+    }
+
+    @Override
+    public UserData getUser(UserData user) {
+        return null;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    private final String[] create = {
+
+    }
+
+    private void configureDatabase() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()){
+            for (String line : create){
+                try (var preppedStatement = conn.prepareStatement(line)){
+                    preppedStatement.executeUpdate();
+                }
+            }
+        }
+        catch (SQLException ex) {
+            throw new DataAccessException(String.format("Unable to configure database: %s", ex.getMessage()));
+        }
+    }
 }
